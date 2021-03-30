b2fe8be45821f9e8625b91217d8f6f816aa8ad18
grobbier <elder.iam2@gmail.com>
2021-03-30 10:46:04 -0600

    A lil change

--- src/main/scala/joboperation/SplitColumnOperator.scala
+++ src/main/scala/joboperation/SplitColumnOperator.scala
@@ -7,17 +7,15 @@ object SplitColumnOperator {
 
   val extractor: Extractor = new Extractor
   val LOGGER: Logger = LogManager
-    .getLogger("SantanderTest")
+    .getLogger("Split Column App")
 
   def main(args: Array[String]): Unit = {
 
     val pathInput = args(0)
     val delimiter = args(1)
-    //val pathInput = "src/main/resources/data/datasetWithPipe.csv"
-    //val pathInput = "src/main/resources/data/query-hive-84190.csv"
     LOGGER.info(s"Read Dataset from $pathInput with the next delimiter $delimiter")
     val originalDF = extractor.readCSV(pathInput, delimiter)
-    LOGGER.info(s"Splitting Columns from $originalDF")
+    LOGGER.info(s"Split Columns from $originalDF")
     val splitDF = extractor.splitColumn(originalDF , delimiter)
     LOGGER.info(s"Doing a join with $originalDF and $splitDF")
     extractor.joinDataFrames(originalDF,splitDF)
